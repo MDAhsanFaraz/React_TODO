741e300920068ac5a2142f511e8f4b17b100f15f
Faraz <faraz.com>
2024-02-14 08:57:43 +0530

    add Edit and finish feature

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,6 +5,24 @@ function TodoList({ todos, setTodos }) {
     const newTodoList = todos.filter((todo) => todo.id != id);
     setTodos(newTodoList);
   }
+  function onEditTodo(id, newTodo) {
+    const newTodoList = todos.map((todo) => {
+      if (todo.id == id) {
+        todo.text = newTodo;
+      }
+      return todo;
+    });
+    setTodos(newTodoList);
+  }
+  function onFinishTodo(id, state) {
+    const newTodoList = todos.map((todo) => {
+      if (todo.id == id) {
+        todo.isFinished = state;
+      }
+      return todo;
+    });
+    setTodos(newTodoList);
+  }
   return (
     todos &&
     todos.map((todo) => (
@@ -14,6 +32,8 @@ function TodoList({ todos, setTodos }) {
         id={todo.id}
         isFinished={todo.isFinished}
         deleteTodo={() => onDeleteTodo(todo.id)}
+        editTodo={(newTodo) => onEditTodo(todo.id, newTodo)}
+        finishTodo={(state) => onFinishTodo(todo.id, state)}
       />
     ))
   );
