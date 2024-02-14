d09c4989c109425834873cb701ef7186606873d5
Faraz <faraz.com>
2024-02-14 08:36:05 +0530

    added ondelete custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "./App.css";
+import AddTodo from "./components/AddTodo/AddTodo";
 import TodoList from "./components/TodoList/TodoList";
 
-const todos = [
-  { id: 1, text: "todo1", isFinished: true },
-  { id: 2, text: "todo2", isFinished: false },
-];
-
 function App() {
-  return <TodoList todos={todos} />;
+  const [todos, setTodos] = useState([
+    { id: 1, text: "todo1", isFinished: true },
+    { id: 2, text: "todo2", isFinished: false },
+  ]);
+
+  function addTodos(todoText) {
+    let nextId = todos.length + 1;
+    setTodos([...todos, { id: nextId, isFinished: false, text: todoText }]);
+  }
+  return (
+    <>
+      <AddTodo addTodos={addTodos} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function AddTodo({ addTodos }) {
+  const [todoText, setTodoText] = useState("");
+  return (
+    <>
+      <input
+        placeholder="add your next todo..."
+        onChange={(e) => {
+          setTodoText(e.target.value);
+        }}
+        value={todoText}
+      />
+      <button
+        onClick={() => {
+          addTodos(todoText);
+          setTodoText("");
+        }}
+      >
+        Submit
+      </button>
+    </>
+  );
+}
+export default AddTodo;
