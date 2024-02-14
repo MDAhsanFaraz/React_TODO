f2f49769cbf770512651696ac1f642989f6d10cd
Faraz <faraz.com>
2024-02-14 07:36:06 +0530

    added Todolist & todo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import TodoList from "./components/TodoList/TodoList";
+
+const todos = [
+  { id: 1, text: "todo1", isFinished: true },
+  { id: 2, text: "todo2", isFinished: false },
+];
 
 function App() {
-  return <>Todos</>;
+  return <TodoList todos={todos} />;
 }
 
 export default App;
